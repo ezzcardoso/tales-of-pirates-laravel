785461fa117f60eac62a5b4f3e74fa75a2342dc4
ezequiel dos anjos cardoso <ezequiel-ac@outlook.com>
2018-08-10 00:01:13 -0300

    0.2.0

---
+++ app/Http/Controllers/AccountGamerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AccountGamer;
+use Illuminate\Http\Request;
+
+class AccountGamerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AccountGamer  $accountGamer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AccountGamer $accountGamer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AccountGamer  $accountGamer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AccountGamer $accountGamer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AccountGamer  $accountGamer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AccountGamer $accountGamer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AccountGamer  $accountGamer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AccountGamer $accountGamer)
+    {
+        //
+    }
+}
